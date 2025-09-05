82f25f28af22665aa22f3255e50851f1d3cf67d3
danielsj2022 <Jeremiahdaniels2004@gmail.com>
2025-09-05 12:16:17 -0400

    full creation of patient and listing all patients Closed 1

--- CLI.Healthcare/Program.cs
+++ CLI.Healthcare/Program.cs
@@ -33,7 +33,9 @@ namespace CLI.Healthcare
                 switch(userChoice){
                     case "c1":
                     case "C1":
-
+                        Patient newPatient = CreatePatient();
+                        //show patient id
+                        patients.Add(newPatient);
                         break;
                     case "c2":
                     case "C2":
@@ -43,6 +45,9 @@ namespace CLI.Healthcare
                         break;
                     case "l1":
                     case "L1":
+                        foreach(var p in patients){
+                            Console.WriteLine(p);
+                        }
                         break;
                     case "l2":
                     case "L2":
@@ -80,42 +85,50 @@ namespace CLI.Healthcare
             } while(quitChoice);
         }
 
-        public void createPatient(){
+        public static Patient CreatePatient(){
             //Patient(string name, string address, string bday, string race, string gender, string diagnosis)
             Console.Write("Enter name: ");
             string? name = Console.ReadLine();
-            while(name == null){
+            while(string.IsNullOrEmpty(name)){
                 Console.Write("Enter valid name: ");
                 name = Console.ReadLine();
             }
             Console.Write("Enter address: ");
             string? address = Console.ReadLine();
-            while(address == null){
+            while(string.IsNullOrEmpty(address)){
                 Console.Write("Enter valid address: ");
                 address = Console.ReadLine();
             }
             Console.Write("Enter birthday(##/##/####): ");
             string? bday = Console.ReadLine();
-            while(bday == null){
+            while(string.IsNullOrEmpty(bday)){
                 Console.Write("Enter valid birthday: ");
                 bday = Console.ReadLine();
             }
             Console.Write("Enter race: ");
             string? race = Console.ReadLine();
-            while(race == null){
+            while(string.IsNullOrEmpty(race)){
                 Console.Write("Enter valid race: ");
                 race = Console.ReadLine();
             }
             Console.Write("Enter gender: ");
             string? gender = Console.ReadLine();
-            while(gender == null){
+            while(string.IsNullOrEmpty(gender)){
                 Console.Write("Enter valid gender: ");
                 gender = Console.ReadLine();
             }
             Console.Write("Enter diagnosis (leave blank if empty): ");
             string? diagnosis = Console.ReadLine();
+            if(string.IsNullOrEmpty(diagnosis)){
+                diagnosis="none";
+            }
+            Console.Write("Enter prescription (leave blank if empty): ");
+            string? prescript = Console.ReadLine();
+            if(string.IsNullOrEmpty(prescript)){
+                prescript="none";
+            }
             //here
-            Patient pt = new Patient();
+            return new Patient(name, address, bday, race, gender, diagnosis, prescript);
         }
     }
 }
--- Library.Healthcare/Models/Patient.cs
+++ Library.Healthcare/Models/Patient.cs
@@ -5,15 +5,16 @@ namespace Library.Healthcare.Models;
 public class Patient
 {
     public int PatientId {get; set;}
-    public required string Name {get; set;}
-    public required string Address {get; set;}
-    public required string Birthday {get; set;}
-    public required string Race {get; set;}
-    public required string Gender {get; set;}
-    public string? Diagnosis {get; set;}
-    List<string?> prescriptions = new List<string?>();
+    public string Name {get; set;}
+    public string Address {get; set;}
+    public string Birthday {get; set;}
+    public string Race {get; set;}
+    public string Gender {get; set;}
+    public string Diagnosis {get; set;}
+    //List<string?> prescriptions = new List<string?>();
+    public string Prescription{get; set;}
 
-    public Patient(string name, string address, string bday, string race, string gender, string diagnosis){
+    public Patient(string name, string address, string bday, string race, string gender, string diagnosis, string prescript){
         var rand = new Random();
         PatientId = rand.Next();
         Name = name;
@@ -22,8 +23,14 @@ public class Patient
         Race = race;
         Gender = gender;
         Diagnosis = diagnosis;
-
+        Prescription = prescript;
     }
 
+    public override string ToString()
+    {
+        string text = $"PatientId: {PatientId}\nName: {Name}\nAddress: {Address}\nBirthday: {Birthday}\n";
+        text += "Race: {Race}\tGender: {Gender}\nDiagnosis: {Diagnosis}\nPrescription: {Prescription}\n";
+        return text;
+    }
     //workout perscript
 }
